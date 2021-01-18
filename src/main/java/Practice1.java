bd227911e482e53ca7e59e1b14e30d9c758ff4a3
chentao <515923117@qq.com>
2021-01-18 22:17:31 +0800

    机械好垃圾

--- src/main/java/Practice1.java
+++ src/main/java/Practice1.java
@@ -2,5 +2,6 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("辽宁工业大学");
         System.out.println("机械设计制造及其自动化");
+        System.out.println("工资3000+");
     }
 }
